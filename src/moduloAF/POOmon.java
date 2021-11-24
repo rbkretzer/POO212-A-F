995eb2476666dc8a0ce7faf46003dd476639389f
João Guilherme Oliani <jguilherme.oliani@gmail.com>
2021-11-23 22:33:00 -0300

    Alteracoes para entrega dia 24/11

--- src/moduloAF/POOmon.java
+++ src/moduloAF/POOmon.java
@@ -1,6 +1,10 @@
 package moduloAF;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -9,31 +13,31 @@ import moduloAGame.Mediator;
 import moduloAGame.POOmonBehavior;
 
 public class POOmon implements POOmonBehavior {
-	
+
 	private String name;
-	
+
 	private int activations;
-	
+
 	private int energy;
-	
+
 	private String history;
-	
+
 	private ImageIO image;
-	
+
 	private int highestEnergy;
-	
+
 	private LocalDateTime highestEnergyTime;
-	
+
 	private boolean lostGame;
-	
+
 	private int wins;
-	
+
 	private Env environment;
-	
+
 	private Mediator mediator;
-	
+
 	private int game;
-	
+
 	public POOmon(String name, String history, ImageIO image, Env env) {
 		setName(name);
 		setHistory(history);
@@ -41,15 +45,15 @@ public class POOmon implements POOmonBehavior {
 		setEnvironment(env);
 		setEnergy(500);
 	}
-	
+
 	public void setGame(int game) {
 		this.game = game;
 	};
-	
+
 	public void serLostGame(boolean lostGame) {
 		this.lostGame = lostGame;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -77,11 +81,11 @@ public class POOmon implements POOmonBehavior {
 	public void setHighestEnergyTime(LocalDateTime highestEnergyTime) {
 		this.highestEnergyTime = highestEnergyTime;
 	}
-	
+
 	public void setEnvironment(Env env) {
 		this.environment = env;
 	}
-	
+
 	public void addWin() {
 		this.wins += 1;
 	}
@@ -138,25 +142,55 @@ public class POOmon implements POOmonBehavior {
 		}
 		return false;
 	}
-	
+
 	public boolean getLostGame() {
 		return this.lostGame;
 	}
-	
+
 	public int getGame() {
 		return this.game;
 	}
-	
+
 	@Override
 	public void attack(POOmonBehavior arg0, Env arg1) {
-		// TODO tirar dúvida com o professor
 		int damage = 0;
-		if (arg1.equals(this.environment)) {
-			damage = Math.round(damage * 1.20f);
+		int damageboost = 0;
+		String typeattacktext = "";
+		Random randomint = new Random();
+		int typeAttack = randomint.nextInt(3 - 1 + 1) + 1;
+		int energyspent = 0;
+		if (typeAttack == 3) {
+			energyspent = randomint.nextInt(200 - 100 + 1) + 100;
+			damage = Math.round(energyspent * 1.50f);
+			typeattacktext = "Cruel";
+		} else
+
+		if (typeAttack == 2) {
+			energyspent = randomint.nextInt(99 - 40 + 1) + 40;
+			damage = Math.round(energyspent);
+			typeattacktext = "Agressivo";
+		} else {
+			damage = 30;
+			typeattacktext = "Básico";
 		}
-		
+		if (typeAttack == 3 || typeAttack == 2) {
+			this.energy -= damage;
+		}
+		if (arg1.equals(this.environment)) {
+			damageboost = Math.round(damage * 0.20f);
+		}
+		arg0.takeDamage(damage + damageboost, arg1);
+
+		try {
+			FileWriter fw = new FileWriter(this.name + "Log.txt");
+			BufferedWriter writer = new BufferedWriter(fw);
+			writer.write("Ataque Efetuado: " + typeattacktext + " " + damage + "(" + (damage + damageboost) + ") - "
+					+ arg1.toString() + "(-" + energyspent + ")");
+		} catch (IOException ieo) {
+			ieo.printStackTrace();
+		}
 	}
-	
+
 	@Override
 	public void restoreEnergy(int arg0) {
 		setEnergy(arg0);
@@ -164,14 +198,23 @@ public class POOmon implements POOmonBehavior {
 
 	@Override
 	public void setMediator(Mediator arg0) {
-		// TODO tirar dúvida com o professor
 		this.mediator = arg0;
 	}
 
 	@Override
 	public void takeDamage(int arg0, Env arg1) {
-		if(arg1.equals(this.environment)) {
-			this.energy -= Math.round(arg0 * 0.90f);
+		int damage = arg0;
+		if (arg1.equals(this.environment)) {
+			damage = Math.round(arg0 * 0.90f);
+		}
+		this.energy -= damage;
+
+		try {
+			FileWriter fw = new FileWriter(this.name + "Log.txt");
+			BufferedWriter writer = new BufferedWriter(fw);
+			writer.write("Ataque Recebido: " + arg0 + " - " + arg1.toString() + "(-" + damage + ")");
+		} catch (IOException ieo) {
+			ieo.printStackTrace();
 		}
 	}
 
